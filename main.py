9773bcbc15bcd363d06e11717a2f924eb634a8b3
Antonio Oliveira - DEV <63135415+3636-sergioharpazo@users.noreply.github.com>
2023-06-16 21:31:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,54 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+livros=[{
+'id':1,
+'titulo':'A vida digital',
+'Autor':'Antonio Oliveira'
 
+},{
+
+'id':2,
+'titulo':'A vida digital2',
+'Autor':'Antonio Oliveira2'
+}
+]
+@app.route('/livros/')
+def obter_livros():
+    return jsonify(livros)
+
+@app.route('/livros/<int:id>',methods=['GET'])
+def consulta_livros_id(id):
+    for livro in livros:
+        if livro.get('id')==id:
+            return jsonify(livro)
+
+@app.route('/livros/<int:id>',methods=['PUT'])
+def editar_livros_id(id):    
+    livro_novo=request.get_json()
+    for indece, livro in enumerate(livros):
+        if livro.get('id')==id:
+            livros[indece].update(livro_novo)
+            return jsonify(livros[indece]) 
+        
+@app.route('/livros/<int:id>',methods=['DELETE'])
+def delete_livros_id(id):    
+    livro_novo=request.get_json()
+    for indece, livro in enumerate(livros):
+        if livro.get('id')==id:
+            del livros[indece]
+            return jsonify(livros) 
+
+@app.route('/livros',methods=['POST'])
+def criar_livros_id():    
+    livro_novo=request.get_json()
+    livros.append(livro_novo) 
+    return jsonify(livros) 
+
+
+            
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
